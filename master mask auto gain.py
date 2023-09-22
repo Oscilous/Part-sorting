6c174e651bdb58f085c47c5a145a2e8cf059b0fb
oscilous <arfil21@student.sdu.dk>
2023-09-22 10:41:07 +0100

    Removed a duplicate import line

--- master mask auto gain.py
+++ master mask auto gain.py
@@ -8,9 +8,8 @@ import sys
 import smtplib
 
 from picamera.array import PiRGBArray
-from picamera import PiCamera 
-
 from picamera import PiCamera
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
